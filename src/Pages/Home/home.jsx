cb7976e69d66ef1d4ad64ee509218e7f1f45af81
VanMendesvs <v_mendesv@hotmail.com>
2024-11-22 14:03:07 -0300

    First Commit

---
+++ src/Pages/Home/home.jsx
@@ -0,0 +1,11 @@
+import S from "./home.module.scss"
+
+export default function Home (){
+    return(
+        <section>
+            <section className={S.post}>
+            <h2>Venha fazer parte da maior rede de doação</h2>
+            </section>
+            </section>
+    )
+}
---
+++ src/components/Header/header.jsx
@@ -0,0 +1,38 @@
+import Logo from "../../assets/logo.png"
+import Busca from "../../assets/search.png"
+import Home from "../../Pages/Home/home"
+import Doados from "../../Pages/Doados/doados"
+import QueroDoar from "../../Pages/QueroDoar/querodoar"
+import S from "./header.module.scss"
+
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
+
+export default function Header() {
+    return(
+      <BrowserRouter>
+      <header>
+       <section className={S.boxLogo}>
+        <img src={Logo} alt="Imagem de Livro Aberto logotipo da empresa" />
+        <h1>Livros Vai na Web</h1>
+       </section>   
+       <nav className={S.boxMenu}>
+        <ul>
+            <li><Link to="/">Inicio</Link></li>
+            <li><Link to="/doados">Livros Doados</Link></li>
+            <li><Link to="/queroDoar">Quero Doar</Link></li>
+        </ul>
+       </nav>
+       <div className={S.boxSearch}>
+       <input className={S.boxInput} type="text" placeholder="O que você procura?" />
+       <img src= {Busca} alt="" />
+       </div>
+       
+      </header>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/doados" element={<Doados/>}/>
+        <Route path="/queroDoar" element={<QueroDoar/>}/>
+      </Routes>
+      </BrowserRouter>
+    )
+  }
