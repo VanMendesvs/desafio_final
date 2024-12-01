ea020aa407dc315b2c0f77387fedc2c01e952c02
VanMendesvs <v_mendesv@hotmail.com>
2024-12-01 19:02:00 -0300

    Atualizado Doados e inicio responsivo

--- src/Pages/Doados/doados.jsx
+++ src/Pages/Doados/doados.jsx
@@ -1,7 +1,19 @@
+import S from "./doados.module.scss"   
+import Capa from "../../assets/capaLivro.png"
+
+
 export default function Doados (){
     return(
-        <section>
-            <h2>Doados</h2>
+        <section className={S.boxDoados}>
+            <h2>Livros Doados</h2>
+            <section className={S.boxCards}>
+                <article>
+                    <img src={Capa} alt="Capa do Livro com titulo Protagonista"/>
+                    <h3>Protagonista</h3>
+                    <p>Susane Andrade</p>
+                    <p>Ficção</p>
+                </article>
+            </section>
         </section>
     )
 }
